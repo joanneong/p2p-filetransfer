0c149c6bb6392aef1685ce99ea1fdc5a2e6b22ce
Chen Xiaoman <836932889@qq.com>
2018-10-21 15:30:41 +0800

    Add tables in directory server

--- src/DirectoryServer.java
+++ src/DirectoryServer.java
@@ -1,12 +1,23 @@
+import java.util.HashMap;
+
 public class DirectoryServer {
 
-    // TODO: tables in server
+    private HashMap<Chunk, Host> firstTable;
+    private HashMap<Host, Chunk> secondTable;
+
+    /**
+     * Constructor
+     */
+    public DirectoryServer() {
+        this.firstTable = new HashMap<>();
+        this.secondTable = new HashMap<>();
+    }
 
     private String getAckMessage() {
         return "";
     }
 
-    private String getQueryReplyMessage(String fileName, int chunkNumber) {
+    private String getQueryReplyMessage(Chunk chunk) {
         return "";
     }
 
@@ -34,6 +45,62 @@ public class DirectoryServer {
     public static void main(String[] args) {
         DirectoryServer directoryServer = new DirectoryServer();
         directoryServer.start();
+    }
+
+    private class Chunk {
+        private String filename;
+
+        private int chunkNumber;
+
+        public Chunk(String filename, int chunkNumber) {
+            this.filename = filename;
+            this.chunkNumber = chunkNumber;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public int getChunkNumber() {
+            return chunkNumber;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other == this
+                    || (other instanceof Host
+                    && this.chunkNumber == ((Chunk) other).chunkNumber
+                    && this.filename.equals(((Chunk) other).filename));
+
+        }
+
+        @Override
+        public int hashCode() {
+            return this.filename.hashCode() + this.chunkNumber * 17;
+        }
+    }
+
+    private class Host {
+        private String IPAddress;
+        private int portNumber;
+
+        public Host(String IPAddress, int portNumber) {
+            this.IPAddress = IPAddress;
+            this.portNumber = portNumber;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other == this
+                    || (other instanceof Host
+                    && this.portNumber == ((Host) other).portNumber
+                    && this.IPAddress.equals(((Host) other).IPAddress));
+        }
+
+        @Override
+        public int hashCode() {
+            return this.IPAddress.hashCode() + this.portNumber * 17;
+        }
     }
 
 }
