7bb8f36fe136da680d05bd09ef133d606c90030e
Chen Xiaoman <836932889@qq.com>
2018-10-21 15:03:08 +0800

    Add skeleton code

---
+++ src/P2PTransientServer.java
@@ -0,0 +1,18 @@
+public class P2PTransientServer {
+    
+    private String getReplyMessage(String fileName, int chunkNumber) {
+        return "";
+    }
+
+    private void send(String messageToSend, String IPAddress, int port) {
+
+    }
+
+    private void start() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
