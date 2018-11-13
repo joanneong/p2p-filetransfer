af70705c0fde40de3c2201d9d4e025219c6b902a
Chen Xiaoman <836932889@qq.com>
2018-11-13 10:27:19 +0800

    Remove _ from query filesize

--- src/Constant.java
+++ src/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     public static final String COMMAND_NAME = "NAME";
     public static final String COMMAND_UPLOAD = "UPLOAD";
     public static final String COMMAND_INFORM_FILESIZIE = "FILESIZE";
-    public static final String COMMAND_QUERY_FILESIZIE = "QUERY_FILESIZE";
+    public static final String COMMAND_QUERY_FILESIZIE = "QUERYFILESIZE";
 
     public static final String TYPE_CLIENT_SOCKET = "CLIENT";
     public static final String TYPE_TRANSIENT_SOCKET = "TRANSIENT";
